2a7ce9a9441de81538d6b4c892e026d3bb3758fc
Mischa Radenovic <miroslav.radenovic@gmail.com>
2019-03-09 12:02:18 -0500

    refactor: improve html semantic

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,29 @@
 <template>
   <div id="app" class="section">
-    <Page />
+    <!-- Provider content -->
+    <main>
+      <!-- Provider header -->
+      <header class="content">
+        <sign-out/>
+
+        <sign-in/>
+      </header>
+
+      <!-- Provider filter -->
+      <section class="content">
+        <issue-search/>
+      </section>
+
+      <!-- Provider error -->
+      <section class="content">
+        <error/>
+      </section>
+
+      <!-- Provider filter result -->
+      <section class="content">
+        <Page />
+      </section>
+    </main>
   </div>
 </template>
 
@@ -8,11 +31,19 @@
 import 'font-awesome/css/font-awesome.css'
 import './assets/styles/issuantt.scss'
 
+import SignIn from './components/SignIn.vue'
+import SignOut from './components/SignOut.vue'
+import IssueSearch from './components//IssueSearch.vue'
+import Error from './components//Error.vue'
 import Page from './components/Page.vue'
 
 export default {
   name: 'app',
   components: {
+    SignIn,
+    SignOut,
+    IssueSearch,
+    Error,
     Page
   }
 }
--- src/components/Page.vue
+++ src/components/Page.vue
@@ -1,44 +1,28 @@
 <template>
   <div>
-    <section class="content">
-
-      <sign-out/>
-
-      <sign-in/>
-    </section>
-
-    <section class="content">
-      <issue-search/>
-      <error/>
-
-      <div v-if="issues && issues.length">
+    <div v-if="issues && issues.length">
+      <nav>
         <pagination></pagination>
+      </nav>
 
-        <tasks :issues="issues"/>
+      <tasks :issues="issues"/>
 
+      <nav>
         <pagination></pagination>
-      </div>
-    </section>
+      </nav>
+    </div>
   </div>
 </template>
 
 <script>
 import Pagination from './Pagination.vue'
-import SignIn from './SignIn.vue'
-import SignOut from './SignOut.vue'
-import IssueSearch from './IssueSearch.vue'
-import Error from './Error.vue'
 import Tasks from './Tasks.vue'
 
 export default {
   name: 'page',
   components: {
     Tasks,
-    Error,
-    Pagination,
-    SignIn,
-    SignOut,
-    IssueSearch
+    Pagination
   },
   computed: {
     issues () {
